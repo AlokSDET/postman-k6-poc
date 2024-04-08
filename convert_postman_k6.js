bb09789138528037eabbbe8a05331320f8568c9c
alok shrivastava <alok.shrivastava@doubleverify.com>
2024-04-08 14:38:49 +0530

    commit with data restructured

--- convert_postman_k6.js
+++ convert_postman_k6.js
@@ -2,33 +2,40 @@ const fs = require('fs')
 const { exec } = require('child_process');
 
 const path = require('path');
- 
 
-const enviornment = process.argv.slice(2)
+
+const enviornment = process.argv.slice(2)[0]
+console.log(enviornment)
+const test_data = process.argv.slice(2)[1]
+console.log(test_data)
 // Path to the collections folder
 const COLLECTIONS_FOLDER = path.join(__dirname, 'collections');
-const K6_SCRIPT_FOLDER = path.join(__dirname, 'k6_scripts');
+const COLLECTIONS_FOLDER_DATA = path.join(__dirname, 'collections', 'collections_with_data');
+const COLLECTIONS_FOLDER_WITHOUT_DATA = path.join(__dirname, 'collections', 'collections_withOUT_data');
+let K6_SCRIPT_FOLDER = path.join(__dirname, 'k6_scripts');
 
 if (fs.existsSync(K6_SCRIPT_FOLDER)) {
-  fs.rmdirSync(K6_SCRIPT_FOLDER, {recursive: true})
+  fs.rmdirSync(K6_SCRIPT_FOLDER, { recursive: true })
 }
 
+K6_SCRIPT_FOLDER = path.join(__dirname, 'k6_scripts');
+console.log(K6_SCRIPT_FOLDER)
 
 
 // Read the contents of the collections folder
-fs.readdir(COLLECTIONS_FOLDER, (err, files) => {
+fs.readdir(COLLECTIONS_FOLDER_WITHOUT_DATA, (err, files) => {
   if (err) {
     console.error('Error reading folder:', err);
     return;
   }
- 
+
   // Iterate through each file in the folder
   files.forEach(file => {
     console.log(file)
     // Check if the file is a JSON file
     if (file.endsWith('.json')) {
       // Execute postman-to-k6 for each JSON file
-      exec(`postman-to-k6 ${COLLECTIONS_FOLDER}/${file} -e ./enviornments/${enviornment}.json  -o ${K6_SCRIPT_FOLDER}/k6_${file.slice(0, -5)}.js`, (error, stdout, stderr) => {
+      exec(`postman-to-k6 ${COLLECTIONS_FOLDER_WITHOUT_DATA}/${file} -e ./enviornments/${enviornment}.json -o ${K6_SCRIPT_FOLDER}/k6_${file.slice(0, -5)}.js`, (error, stdout, stderr) => {
         if (error) {
           console.error(`Error converting ${file} to k6 script:`, error);
           return;
@@ -36,5 +43,44 @@ fs.readdir(COLLECTIONS_FOLDER, (err, files) => {
         console.log(`Conversion for ${file} completed successfully`);
       });
     }
-  });
+  })
+});
+
+
+// Read the contents of the collections folder
+fs.readdir(COLLECTIONS_FOLDER_DATA, (err, files) => {
+  if (err) {
+    console.error('Error reading folder:', err);
+    return;
+  }
+
+  // Iterate through each file in the folder
+  files.forEach(file => {
+    console.log(file)
+    // Check if the file is a JSON file
+    if (file.endsWith('.json')) {
+      if (test_data.includes('.json')) {
+        console.log('json test data')
+        // Execute postman-to-k6 for each JSON file
+        exec(`postman-to-k6 ${COLLECTIONS_FOLDER_DATA}/${file} -e ./enviornments/${enviornment}.json  --json ./testdata/${test_data} -o ${K6_SCRIPT_FOLDER}/k6_${file.slice(0, -5)}.js`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error converting ${file} to k6 script:`, error);
+            return;
+          }
+          console.log(`Conversion for ${file} completed successfully`);
+        });
+
+      } if (test_data.includes('.csv')) {
+        console.log('csv test data')
+        // Execute postman-to-k6 for each JSON file
+        exec(`postman-to-k6 ${COLLECTIONS_FOLDER_DATA}/${file} -e ./enviornments/${enviornment}.json  --csv ./testdata/${test_data} -o ${K6_SCRIPT_FOLDER}/k6_${file.slice(0, -5)}.js`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error converting ${file} to k6 script:`, error);
+            return;
+          }
+          console.log(`Conversion for ${file} completed successfully`);
+        });
+      }
+    }
+  })
 });
